b6394ef6bd2168c9902b3ef5ee93484d8ba3432f
lee-jh0208 <as97164@naver.com>
2021-02-08 15:56:48 +0900

    이거 된거 맞아요? 제발류~~~

---
+++ Anjfkrhwjrdj/src/com/base/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.base;
+
+public class HelloWorld {
+	int age = 20;
+
+	public static void main(String[] args) {
+		
+		HelloWorld ag = new HelloWorld();
+		ag.age =28;
+		System.out.println("당신의 나이는"+ag.age+"입니다");
+	}
+
+}
